104b4f9b04598222d35bde9a70a7253eae58e34f
sudip <soodip.kafle@gmail.com>
2016-01-14 11:54:51 +0545

    updated for python 3

--- jobscraper/scraper.py
+++ jobscraper/scraper.py
@@ -7,22 +7,11 @@ UPWORK_FEED = "https://www.upwork.com/jobs/rss?q={0}"
 
 
 def scrape_content(title):
-    hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11'
-        ' (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9'
-        ',*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'
-    }
     # params = urllib.urlencode({'q': title, 'sort': 'create_time+desc'})
     myurl = UPWORK_FEED.format(urllib.quote(title))
-    print "url is", myurl
+    print("url is", myurl)
     # try:
-    req = requests.get(
-        myurl, headers=hdr)
+    req = requests.get(myurl)
     return req.text
 
 
