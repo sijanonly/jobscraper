60f277a48f8e2160c8de7ee3f74674ea7df9898e
sudip <soodip.kafle@gmail.com>
2016-01-14 08:37:25 +0545

    used rss feed instead of scraping

--- jobscraper/scraper.py
+++ jobscraper/scraper.py
@@ -1,6 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
 import urllib
+import xmltodict
+
+
+UPWORK_FEED = "https://www.upwork.com/jobs/rss?q={0}"
 
 
 def scrape_content(title):
@@ -14,41 +17,18 @@ def scrape_content(title):
         'Accept-Language': 'en-US,en;q=0.8',
         'Connection': 'keep-alive'
     }
-    params = urllib.urlencode({'q': title, 'sort': 'create_time+desc'})
-    myurl = "https://www.upwork.com/o/jobs/browse/?%s" % params
+    # params = urllib.urlencode({'q': title, 'sort': 'create_time+desc'})
+    myurl = UPWORK_FEED.format(urllib.quote(title))
     print "url is", myurl
     # try:
     req = requests.get(
         myurl, headers=hdr)
     return req.text
-    # except:
-        # raise
 
 
 def scrape(job):
     jobs = scrape_content(job)
-    soup = BeautifulSoup(jobs, "html.parser")
-    # print "jobs are", soup
-    job_body = soup.find("div", {"id": "jobs-list"})
-    jobs = job_body.find_all('article')
-    # print "all jobs are", jobs
-    total_jobs = {}
-    for each_job in jobs:
-        job_dict = {}
-        job_link = each_job.find('a')
-        dict_id = job_link['data-position']
-        job_dict['title'] = job_link.text.strip()
-        # print "job_link", job_link['href']
-        job_href = job_link['href']
-        job_href = job_href.split('/')
-        job_href = job_href[4]
-        job_dict['link'] = "https://www.upwork.com/jobs/_" + job_href
-        total_jobs[dict_id] = job_dict
-
-    # job_link = jobs[0].find('a')
-    # print "job_link", job_link['href'], job_link.text.strip()
-    # print "job_dict is", total_jobs
-    return total_jobs
+    return dict(xmltodict.parse(jobs))
 
 
 if __name__ == '__main__':
